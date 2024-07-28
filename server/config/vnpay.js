978615fa27fa122e23cdb01bbbb364338fc50b6d
Thaoquyenpo <thiquyen2292@gmail.com>
2024-07-28 16:02:13 +0700

    finalproject

---
+++ server/config/vnpay.js
@@ -0,0 +1,7 @@
+export const vnpay = {
+  vnp_TmnCode: 'M6QZ7N4W',
+  vnp_HashSecret: 'RONLBN1P21F9IGJL1NZF5CTP4ITWTH7V',
+  vnp_Url: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
+  vnp_Api: 'https://sandbox.vnpayment.vn/merchant_webapi/api/transaction',
+  vnp_ReturnUrl: 'https://lq-camera-shop.vercel.app/vnpay_return',
+};
